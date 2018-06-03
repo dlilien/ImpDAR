d2fc94e7a07d87aa0d931697488288e823d1dbc3
dlilien <skinonframe@gmail.com>
2018-06-03 13:02:06 -0700

    All versions of horizontal filters are pythonized

--- impdar/lib/horizontal_filters.py
+++ impdar/lib/horizontal_filters.py
@@ -1,5 +1,5 @@
 import numpy as np
-from scipy.signal import filtfilt
+from scipy.signal import filtfilt, butter, tukey
 
 
 def adaptivehfilt(dat, *args, **kwargs):
@@ -112,3 +112,144 @@ def hfilt(dat, ntr1, ntr2, *args, **kwargs):
 
     # set flags structure components
     dat.flags.hfilt = np.ones((2,))
+
+
+def highpass(dat, wavelength, tracespace):
+    #HIGHPASSDEEP - This is NOT a highpass frequency filter--rather it is a
+    # horizontal filter to be used after interpolation because our data now has
+    # constant spacing and a constant time.  Note that this horizontal filter
+    # requires constant trace-spacing in order to be effective.
+    #
+    #   You will want to experiment with the creation of the average trace.
+    #   Ideally choose an area where all reflectors are sloped and relatively
+    #   dim so that they average out while the horizontal noise is amplified.
+    #   Note that generally there is no perfect horizontal filter that will
+    #   work at all depths.  You will have to experiment to get the best
+    #   results for your area of interest.
+    #
+    #	WARNING: Do not use highpassdeep on elevation-corrected data!!! 
+    #
+    #
+    # Created by L. Smith and modified by A. Hagen, 6/15/04.
+    #
+    #Modifications:
+    #    1) Converted to function and added documentation. B. Welch 5/3/06
+    #    2) Added ability to bypass user input when running batch files,
+    #    increased function inputs and outputs. - J. Werner, 6/30/08.
+    #	 3) Added flags structure to function input and output.  Also added
+    #		code to set hfilt components of flags structure.  J. Olson 7/10/08
+    #	 
+
+    #Convert wavelength to meters.
+    wavelength = wavelength * 1000
+    #Set an approximate sampling frequency (10ns ~ 10m --> 100MHz).
+    fsamp = 100
+    #Calculate the number of samples per wavelength.
+    nsamp = (wavelength - (wavelength % tracespace)) // tracespace
+    print('Sample resolution = {:d}'.format(nsamp))
+    #The high corner frequency is the ratio of the sampling frequency (in MHz)
+    #and the number of samples per wavelength (unitless).
+    High_Corner_Freq = fsamp / nsamp
+    print('	Lowpass cutoff at {:4.2f} MHz...'.format(High_Corner_Freq))
+
+    Sample_Freq = 1 / dat.dt
+
+    Nyquist_Freq = Sample_Freq / 2.0
+    #Convert High_Corner_Freq to Hz.
+    High_Corner_Freq = High_Corner_Freq * 1e6
+
+    #Corner_Freq is used in the olaf_butter routine.
+    Corner_Freq = High_Corner_Freq / Nyquist_Freq
+
+    b, a = butter(5, Corner_Freq, 'high')
+
+    dat.data = filtfilt(b, a, dat.data)
+
+    # set flags structure components
+    dat.flags.hfilt[1] = 1
+    dat.flags.hfilt[2] = 3
+
+    print('Highpass filter complete.')
+
+
+def winavg_hfiltdeep(dat, avg_win, taper='full', filtdepth=100):
+    #WINAVG_HFILTDEEP - This StoDeep subroutine performs a horizontal filter on
+    #   the data to reduce the ringing in the upper layers. It uses a moving
+    #   window to creat an average trace for each individual trace, applies an
+    #   exponential taper to it and then subtracts it from the trace. This is
+    #   based off of the original hfiltdeep and uses the moving window designed
+    #   in cosine_win_hfiltdeep.  The extent of the taper can help to minimize
+    #   artifacts that are often produced near regions of bright bed
+    #   reflectors.
+    #
+    #   You will want to experiment with the creation of the average trace.
+    #   Ideally choose an area where all reflectors are sloped and relatively
+    #   dim so that they average out while the horizontal noise is amplified.
+    #   Note that generally there is no perfect horizontal filter that will
+    #   work at all depths.  You will have to experiment to get the best
+    #   results for your area of interest.
+    #
+    #	WARNING: Do not use winavg_hfiltdeep on elevation-corrected data!!!
+    #
+    #Created: Kieran Dwyer 6/18/03
+    #
+    #Modifications:
+    #    1) Coverted to function and added documentation. B. Welch 5/2/06
+    #	 2) Added flags structure to function input and output.  Also added
+    #		code to set hfilt components of flags structure.  J. Olson 7/10/08
+    #	 
+
+    if avg_win % 2 == 0:
+        avg_win = avg_win + 1
+        print('The averaging window must be an odd number of traces.')
+        print('The averaging window has been changed to {:d}'.format(avg_win))
+
+    if taper == 'full':
+        exptaper = np.exp(-dat.travel_time * 0.05) / np.exp(-dat.travel_time[1] * 0.05)
+    elif taper == 'pexp':
+        filtdepth = filtdepth * 100 + dat.trig + 1
+        exptaper = np.exp(-dat.travel_time * 0.05) / np.exp(-dat.travel_time[1] * 0.05)
+        #set taper to effect only the initial times
+        exptaper[1:filtdepth] = exptaper[1:filtdepth] - exptaper[filtdepth]
+        exptaper[filtdepth:dat.snum] = 0
+        exptaper = exptaper / np.max(exptaper)
+    elif taper == 'tukey':
+        exptaper = np.zeros((950,))
+        exptaper[1:30] = np.ones((30,))
+        tukey_win = tukey(60, 0.5)
+        exptaper[31:45] = tukey_win[46:60]
+    else:
+        raise ValueError('Unrecognized taper. Options are full, pexp, or tukey')
+
+    hfiltdata = np.zeros_like(dat.data)
+    #set up ranges, create average, taper average, subtract average
+
+    for i in range(int(dat.tnum)):
+        range_start = i - ((avg_win - 1) / 2)
+        range_end = i + ((avg_win - 1) / 2)
+
+        # wrap range_start around to otherside of window to maintain
+        # constant range size when i is less than 101 and range_start would
+        # be zero or less
+        if range_start < 0:
+            range_end = range_end + range_start
+            range_start = 0
+
+        #wrap range_end around to beginning of window to maintain constant 
+        #range size when range would be greater than tnum, this
+        #perserves size of range.
+        if range_end > dat.tnum:
+            range_start = range_start - (range_end - dat.tnum)
+            range_end = dat.tnum
+
+        #create an average trace
+        avg_trace = (np.sum(dat.data[:, range_start:range_end]) / (range_end - range_start)).flatten() * exptaper
+
+        #subtract avg_trace from each trace
+        hfiltdata[:, i] = dat.data[:, i] - avg_trace
+
+    dat.data = hfiltdata.astype(dat.data.dtype)
+    dat.flags.hfilt[1] = 1
+    dat.flags.hfilt[2] = 2
+
+    print('Horizontal filter complete.')
