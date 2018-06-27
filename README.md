7082d1656475f13cd77092926eb51f4033705474
dlilien <skinonframe@gmail.com>
2018-06-27 12:47:15 -0700

    Update to have a skeleton of working tests for RadarData

--- README.md
+++ README.md
@@ -1,16 +1,21 @@
 # impdar: an impulse radar processor
 
+[![Build Status](https://travis-ci.org/dlilien/impdar.svg?branch=master)](https://travis-ci.org/dlilien/impdar)
+
 This is a re-write of the St. Olaf Deep Radar processor in Python, adding some capability and pruning some dead limbs. This code has a lot of history of contributors: I've tried to preserve acknowledgment of many of them in the file headers.
-
-
-## Installation
 
 ### Dependencies
 
 #### Required
 *Python 2 or 3* Sticking with 3.6 or greater is prudent
 
-*SCIPY*, *NUMPY*, and *MATPLOTLIB*
+[numpy](http://www.scipy.org),
+[scipy](http://numpy.org),
+[matplotlib](http://matplotlib.org)
 
 #### Recommended
 *GDAL* Needed to reproject out of WGS84, and thus for proper distance measurement
+
+## Documentation
+
+Reasonably complete documentation is [here](http://dlilien.github.io/impdar)
--- impdar/tests/test_RadarData.py
+++ impdar/tests/test_RadarData.py
@@ -9,9 +9,12 @@
 """
 Test the basics of RadarData
 """
-
+import os
 import unittest
+import numpy as np
 from impdar.lib.RadarData import RadarData, RadarFlags
+
+THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 class TestFlags(unittest.TestCase):
@@ -20,11 +23,49 @@ class TestFlags(unittest.TestCase):
         self.rdf = RadarFlags()
 
     def test_BoolOutputConversion(self):
+        # Make sure the value is as expected
+        self.rdf.reverse = False
         out = self.rdf.to_matlab()
         self.assertFalse(out['reverse'])
 
+        self.rdf.rgain = True
+        out = self.rdf.to_matlab()
+        self.assertTrue(out['rgain'])
+
+        for attr in self.rdf.attrs:
+            self.assertTrue(attr in out)
+
+    def test_InputConversion(self):
+        
+        in_flags_bad_format = {'agc': 0,
+                               'batch': 0,
+                               'bpass': np.array([0., 0., 0.]),
+                               'crop': np.array([0., 0., 0.]),
+                               'elev': 0,
+                               'hfilt': np.array([0., 0.]),
+                               'interp': np.array([0., 0.]),
+                               'mig': 0,
+                               'nmo': np.array([0., 0.]),
+                               'restack': 0,
+                               'reverse': 0,
+                               'rgain': 0}
+        # in_flags_random_arg = {'unknown_val': False}
+        in_flags_bad = {'reverse': True}
+
+        with self.assertRaises(KeyError):
+            self.rdf.from_matlab(in_flags_bad)
+        with self.assertRaises(TypeError):
+            self.rdf.from_matlab(in_flags_bad_format)
+        # self.rdf.from_matlab(in_flags_random_arg)
+
     def tearDown(self):
         del self.rdf
+
+
+class TestRadarData(unittest.TestCase):
+
+    def test_ReadSucceeds(self):
+        RadarData(os.path.join(THIS_DIR, 'input_data/small_data.mat'))
 
 
 if __name__ == '__main__':
