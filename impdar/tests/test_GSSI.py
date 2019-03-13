c8a3a14a59fa2502df32f3e8e1d326cf5f5a0094
dlilien <skinonframe@gmail.com>
2019-03-13 16:34:25 -0700

    Fixing capitalization error to get GSSI tests passing

--- impdar/tests/test_GSSI.py
+++ impdar/tests/test_GSSI.py
@@ -21,13 +21,13 @@ THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 class Dummy(unittest.TestCase):
 
     def test_load_withDZG(self):
-        load_gssi.DZT(os.path.join(THIS_DIR, 'input_data', 'test_GSSI.DZT'))
+        load_gssi.DZT(os.path.join(THIS_DIR, 'input_data', 'test_gssi.DZT'))
 
     def test_load_withoutDZG(self):
         load_gssi.DZT(os.path.join(THIS_DIR, 'input_data', 'test_gssi_justdzt.DZT'))
 
     def test_save_withDZG(self):
-        load_gssi.DZT(os.path.join(THIS_DIR, 'input_data', 'test_GSSI.DZT')).save(os.path.join(THIS_DIR, 'input_data', 'test_gssi_raw.mat'))
+        load_gssi.DZT(os.path.join(THIS_DIR, 'input_data', 'test_gssi.DZT')).save(os.path.join(THIS_DIR, 'input_data', 'test_gssi_raw.mat'))
         os.remove(os.path.join(THIS_DIR, 'input_data', 'test_gssi_raw.mat'))
 
     def test_save_withoutDZG(self):
