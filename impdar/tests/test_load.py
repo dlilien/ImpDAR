e004018f679dccf43161956156aa1ada2a31a8e0
dlilien <skinonframe@gmail.com>
2019-03-18 10:45:45 -0700

    Test PulseEkko

--- impdar/tests/test_load.py
+++ impdar/tests/test_load.py
@@ -30,6 +30,9 @@ class TestLoad(unittest.TestCase):
     def test_loadgssi(self):
         data = load.load('gssi', os.path.join(THIS_DIR, 'input_data', 'test_gssi.DZT'))
 
+    def test_loadpe(self):
+        data = load.load('pe', os.path.join(THIS_DIR, 'input_data', 'test_pe.DT1'))
+
     def test_loadbad(self):
         with self.assertRaises(ValueError):
             data = load.load('bad', os.path.join(THIS_DIR, 'input_data', 'small_data.bad'))
