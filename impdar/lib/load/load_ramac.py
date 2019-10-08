4151812dfa73778d551dacbcf4d94f95421d7f9a
dlilien <skinonframe@gmail.com>
2019-10-08 11:43:14 +0200

    Loading Greenland 2019 RAMAC data without issue

---
+++ impdar/lib/load/load_ramac.py
@@ -0,0 +1,113 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2019 David Lilien <dlilien90@gmail.com>
+#
+# Distributed under terms of the GNU GPL3.0 license.
+
+"""
+Load RAMAC data
+"""
+
+import os
+import struct
+import numpy as np
+from scipy.interpolate import interp1d
+import datetime
+from ..RadarData import RadarData
+from ..gpslib import nmea_info
+
+
+def load_ramac(ramac_fn):
+    """Return a impdar.lib.RadarData.RadarData object loaded from a Ramac file
+
+    Parameters
+    ----------
+    ramac_fn: str
+        The file to load. Can include the '.rad' or not.
+    """
+    ramac_data = RadarData(None)
+
+    # We can take headers, base filenames, or data files
+    if len(ramac_fn) <= 4:
+        header_fn = ramac_fn + '.rad'
+        data_fn = ramac_fn + '.rd3'
+        gps_fn = ramac_fn + '.cor'
+    elif ramac_fn[-4:] == '.rd3':
+        header_fn = ramac_fn[:-3] + 'rad'
+        data_fn = ramac_fn[:-3] + 'rd3'
+        gps_fn = ramac_fn[:-3] + 'cor'
+    elif ramac_fn[-4:] != '.rad':
+        header_fn = ramac_fn + '.rad'
+        data_fn = ramac_fn + '.rd3'
+        gps_fn = ramac_fn + '.cor'
+    else:
+        header_fn = ramac_fn
+        data_fn = ramac_fn[:-3] + 'rd3'
+        gps_fn = ramac_fn[:-3] + 'cor'
+
+    with open(header_fn) as f_header:
+        header = f_header.readlines()
+
+    ramac_data.chan = ramac_fn[-5]
+    ramac_data.snum = int(header[0].rstrip('\n')[8:])
+    sampling_freq = float(header[1].rstrip('\n')[10:])
+    ramac_data.dt = (1. / sampling_freq) * 1.0e-6
+    # FIX TRACE INT
+    ramac_data.trace_int = float(header[9].rstrip('\n')[14:])
+    dx = int(header[2].rstrip('\n')[16:])
+    if dx == 0:
+        dx = 1
+    ramac_data.tnum = int(header[22].rstrip('\n')[11:])
+    ramac_data.trace_num = np.arange(ramac_data.tnum) + 1
+    ramac_data.travel_time = ramac_data.dt * np.arange(ramac_data.snum) * 1.0e6
+    ramac_data.trig = np.ones((ramac_data.tnum,)) * 36
+    ramac_data.trig_level = 0
+
+    if os.path.exists(gps_fn):
+        cor = np.genfromtxt(gps_fn, dtype=[('trace_num', int),
+                                           ('date', 'S10'),
+                                           ('time', 'S8'),
+                                           ('lat', float),
+                                           ('north', 'S1'),
+                                           ('lon', float),
+                                           ('east', 'S1'),
+                                           ('elev', float),
+                                           ('el_unit', 'S1'),
+                                           ('pdop', float)])
+        datetimes = np.array([d + b'T' + t for d, t in zip(cor['date'], cor['time'])], dtype=np.datetime64)
+        decdays = datetimes - np.array(datetime.datetime(1, 1, 1, 0, 0, 0), dtype=np.datetime64)
+        cor['lat'][cor['north'] != b'N'] = -1 * cor['lat'][cor['north'] != b'N']
+        cor['lon'][cor['east'] != b'E'] = -1 * cor['lon'][cor['east'] != b'E']
+
+        ramac_data.decday = interp1d(cor['trace_num'], decdays, fill_value='extrapolate')(ramac_data.trace_num) / (24. * 60. * 60.)
+        ramac_data.lat = interp1d(cor['trace_num'], cor['lat'], fill_value='extrapolate')(ramac_data.trace_num)
+        ramac_data.long = interp1d(cor['trace_num'], cor['lon'], fill_value='extrapolate')(ramac_data.trace_num)
+        ramac_data.elev = interp1d(cor['trace_num'], cor['elev'], fill_value='extrapolate')(ramac_data.trace_num)
+
+        nminfo = nmea_info()
+        nminfo.time = ramac_data.decday
+        nminfo.lat = ramac_data.lat
+        nminfo.lon = ramac_data.long
+        nminfo.elev = ramac_data.elev
+        nminfo.get_utm()
+        nminfo.get_dist()
+        ramac_data.x_coord = nminfo.x
+        ramac_data.y_coord = nminfo.y
+        ramac_data.dist = nminfo.dist
+
+    else:
+        ramac_data.decday = np.arange(ramac_data.tnum)
+        ramac_data.lat = np.arange(ramac_data.tnum)
+        ramac_data.long = np.arange(ramac_data.tnum)
+        ramac_data.dist = np.arange(ramac_data.tnum)
+        ramac_data.elev = np.arange(ramac_data.tnum)
+    ramac_data.pressure = np.zeros_like(ramac_data.dist)
+
+    with open(data_fn, 'rb') as f_data:
+        ramac_data.data = np.array(struct.unpack('<{:d}h'.format(
+            ramac_data.tnum * ramac_data.snum), f_data.read()[:]),
+            dtype=np.int16).reshape((ramac_data.snum, ramac_data.tnum), order='F')
+    ramac_data.check_attrs()
+    return ramac_data
