60dfad00e59244f14efe823fbd8c44446a5295d0
David Lilien <david.lilien@umanitoba.ca>
2023-11-02 14:46:16 -0400

    Split pypi and test

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ if __name__ == '__main__':
         from Cython.Build import cythonize
         ext_modules = cythonize(ext_modules)
 
-    version = '1.1.8.post3'
+    version = '1.1.8.post4'
     packages = ['impdar',
                 'impdar.lib',
                 'impdar.bin',
